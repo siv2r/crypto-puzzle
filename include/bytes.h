ce3e2e1bf5c739e79327cbdbb3279ab03b59bcaa
siv2r <siv2ram@gmail.com>
2022-01-15 15:09:41 +0530

    initial commit

---
+++ include/bytes.h
@@ -0,0 +1,25 @@
+#ifndef BYTES_H
+#define BYTES_H
+
+#include <stddef.h>
+
+/* Assumptions:
+* The input strings (hex, base64, b32) are assumed to be Big endian type
+* The bytes will be stored in little endian format using `usinged char`
+*/
+
+/* 
+TODO: what is '\x' in C? 
+* used in strings to store hex value in single byte 
+*/
+
+/* prints bufer in hex format */
+void print_buf(const unsigned char *buf, size_t n);
+
+/* converts a single hex character (4 bits) to a byte (8 bits) */
+unsigned char hex_char_to_buf(const unsigned char inp);
+
+/* converts a hex string to a byte array */
+void hex_str_to_buf(unsigned char *out, int out_len, const unsigned char *inp, int inp_len);
+
+#endif
---
+++ src/bytes_impl.h
@@ -0,0 +1,64 @@
+#ifndef BYTES_IMPL_H
+#define BYTES_IMPL_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/util.h"
+
+void print_buf(const unsigned char *buf, size_t n) {
+    size_t i;
+    for (i = 0; i < n; i++) {
+        printf("%02x", buf[i]);
+        if(i % 4 == 3) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+unsigned char hex_char_to_buf(const unsigned char inp) {
+    if (inp >= '0' && inp <= '9') {
+        return inp - '0';
+    } else if (inp >= 'A' && inp <= 'F') {
+        return inp - 'A' + 10;
+    } else if (inp >= 'a' && inp <= 'f') {
+        return inp - 'a' + 10;
+    }
+    fprintf(stderr, "hex string should contain only 0-9,a-f and A-F");
+
+    return -1;
+}
+
+/* given hex string is stored as big endian bytes in unsinged char
+TODO: is there a way to set the out pointer by passing it through arguments?
+TODO: accept an intialized array (memory allocated by the user) and its length
+TODO: assert size
+TODO: load the given hex string to the array
+*/
+void hex_str_to_buf(unsigned char *out, int out_len, const unsigned char *inp, int inp_len) {
+    int i = 0, j = 0, temp = (inp_len + 1)/2; /* output string length */
+    unsigned char low4, high4;
+
+    assert(out_len == temp);
+
+    if (inp_len % 2) {
+        out[j] = hex_char_to_buf(inp[i]);
+        i++;
+        j++;
+    }
+    /* the remaining hex string length will be even */
+    while (i < inp_len && j < out_len) {
+        high4 = hex_char_to_buf(inp[i]);
+        low4 = hex_char_to_buf(inp[i+1]);
+        out[j] = low4 | (high4 << 4);
+
+        i += 2;
+        j++;
+    }
+
+    /*TODO: verify that `out` val is correct? */
+}
+
+#endif /* BYTES_IMPL_H */
